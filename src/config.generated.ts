9848c32a6d566e9eb90416e93591fb010533d0a0
akira921 <akira.lam@defai.digital>
2025-10-28 05:49:59 -0400

    chore: update config.generated.ts from prebuild

--- src/config.generated.ts
+++ src/config.generated.ts
@@ -6,7 +6,7 @@
  *
  * To modify config, edit automatosx.config.json instead.
  *
- * Generated: 2025-10-28T09:44:54.425Z
+ * Generated: 2025-10-28T09:49:25.531Z
  */
 
 import type { AutomatosXConfig } from './types/config.js';
@@ -326,7 +326,7 @@ export const PRECOMPILED_CONFIG: AutomatosXConfig = {
  * Metadata about the precompiled config
  */
 export const PRECOMPILED_CONFIG_META = {
-  generatedAt: '2025-10-28T09:44:54.425Z',
+  generatedAt: '2025-10-28T09:49:25.531Z',
   sourceFile: 'automatosx.config.json',
   version: '5.7.3'
 } as const;

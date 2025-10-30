79a69dfcbf76f5c56cedaeb0ef184fb46cd10d0d
Akira LAM <akiralam@akmp16m3.local>
2025-10-30 08:59:37 -0400

    chore: regenerate config during test run

--- src/config.generated.ts
+++ src/config.generated.ts
@@ -6,7 +6,7 @@
  *
  * To modify config, edit automatosx.config.json instead.
  *
- * Generated: 2025-10-29T21:07:06.812Z
+ * Generated: 2025-10-30T12:59:07.561Z
  */
 
 import type { AutomatosXConfig } from './types/config.js';
@@ -339,7 +339,7 @@ export const PRECOMPILED_CONFIG: AutomatosXConfig = {
  * Metadata about the precompiled config
  */
 export const PRECOMPILED_CONFIG_META = {
-  generatedAt: '2025-10-29T21:07:06.812Z',
+  generatedAt: '2025-10-30T12:59:07.561Z',
   sourceFile: 'automatosx.config.json',
   version: '5.12.0'
 } as const;
